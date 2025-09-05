822840edab9937cfeac140f6966e7bae90e67ec2
maniel <102647979+manielpb@users.noreply.github.com>
2025-09-05 21:18:36 +0000

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -23,3 +23,26 @@ INNER JOIN properties p ON b.property_id = p.property_id
 LEFT JOIN payments pay ON b.booking_id = pay.booking_id
 WHERE b.status = 'Confirmed'
   AND p.location = 'Miami';
+
+
+-- Refactored query using indexes and reduced columns
+EXPLAIN
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.first_name,
+    u.last_name,
+    p.name AS property_name,
+    pay.amount AS payment_amount,
+    pay.payment_date
+FROM bookings b
+INNER JOIN users u 
+    ON b.user_id = u.user_id
+INNER JOIN properties p 
+    ON b.property_id = p.property_id
+LEFT JOIN payments pay 
+    ON b.booking_id = pay.booking_id
+WHERE b.status = 'Confirmed'
+  AND p.location = 'Miami';
