103a45b7d582054913f3c1fb771d25429264af5f
maniel <102647979+manielpb@users.noreply.github.com>
2025-09-05 17:16:20 +0000

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -26,7 +26,8 @@ SELECT
   reviews.comment
 FROM properties
 LEFT JOIN reviews
-ON properties.property_id = reviews.property_id;
+ON properties.property_id = reviews.property_id,
+ORDER BY properties.property_id, reviews.created_at;
 
 // a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 
