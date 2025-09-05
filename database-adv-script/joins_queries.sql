ae7fcae2449ee24b5033f355df9c43ecb4afb321
maniel <102647979+manielpb@users.noreply.github.com>
2025-09-05 17:11:26 +0000

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,47 @@
+// query using an INNER JOIN to retrieve all bookings and the respective users who made those bookings.
+
+SELECT 
+  bookings.booking_id,
+  bookings.start_date,
+  bookings.end_date,
+  bookings.total_price,
+  bookings.status,
+  users.user_id,
+  users.first_name,
+  users.last_name,
+  users.email
+FROM bookings
+INNER JOIN users
+ON bookings.user_id = users.user_id;
+
+// query using a LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
+
+SELECT 
+  properties.property_id,
+  properties.name,
+  properties.location,
+  reviews.review_id,
+  reviews.user_id,
+  reviews.rating,
+  reviews.comment
+FROM properties
+LEFT JOIN reviews
+ON properties.property_id = reviews.property_id;
+
+// a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
+
+SELECT 
+  users.user_id,
+  users.first_name,
+  users.last_name,
+  users.email,
+  bookings.booking_id,
+  bookings.start_date,
+  bookings.end_date,
+  bookings.total_price,
+  bookings.status
+FROM users
+FULL OUTER JOIN bookings
+ON users.user_id = bookings.user_id;
+  
+
